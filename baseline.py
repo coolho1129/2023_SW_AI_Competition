2e374cb9ee6b2f906f304b9936f1bf2101e834eb
j_h_2_0_0_2 <100736860+jaehyeonha@users.noreply.github.com>
2023-07-22 18:42:55 +0900

    import model class

--- baseline.py
+++ baseline.py
@@ -2,8 +2,6 @@ import os
 import cv2
 import pandas as pd
 import numpy as np
-from typing import List, Union
-from joblib import Parallel, delayed
 
 import torch
 import torch.nn as nn
@@ -23,15 +21,10 @@ from albumentations.pytorch import ToTensorV2
 
 import gc
 
-import logging
-
-import torch._utils
-
-import torch.utils.model_zoo as model_zoo
-import ssl
-
-ssl._create_default_https_context = ssl._create_unverified_context # _ssl.c:997 에러처리
-
+from hrnet import HRNet,config
+from unet import UNet
+from deeplabV3plus import DeepLabv3_plus
+from deeplabV3plus_Xception import DeepLabv3_plus_Xception
 
 class SatelliteDataset(Dataset):
     def __init__(self, csv_file, patch_size, stride, transform=None, infer=False):
@@ -81,64 +74,6 @@ def split_mask(mask, patch_size, stride):
             patch = mask[y:y+patch_size, x:x+patch_size]
             patches.append(patch)
     return patches
-
-
-    # 간단한 U-Net 모델 정의
-class UNet(nn.Module):
-    
-    def __init__(self):
-        super(UNet, self).__init__()
-        self.dconv_down1 = self.double_conv(3, 64)
-        self.dconv_down2 = self.double_conv(64, 128)
-        self.dconv_down3 = self.double_conv(128, 256)
-        self.dconv_down4 = self.double_conv(256, 512)
-
-        self.maxpool = nn.MaxPool2d(2)
-        self.upsample = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)        
-
-        self.dconv_up3 = self.double_conv(256 + 512, 256)
-        self.dconv_up2 = self.double_conv(128 + 256, 128)
-        self.dconv_up1 = self.double_conv(128 + 64, 64)
-
-        self.conv_last = nn.Conv2d(64, 1, 1)
-
-    def forward(self, x):
-        conv1 = self.dconv_down1(x)
-        x = self.maxpool(conv1)
-
-        conv2 = self.dconv_down2(x)
-        x = self.maxpool(conv2)
-        
-        conv3 = self.dconv_down3(x)
-        x = self.maxpool(conv3)   
-
-        x = self.dconv_down4(x)
-
-        x = self.upsample(x)        
-        x = torch.cat([x, conv3], dim=1)
-
-        x = self.dconv_up3(x)
-        x = self.upsample(x)        
-        x = torch.cat([x, conv2], dim=1)       
-
-        x = self.dconv_up2(x)
-        x = self.upsample(x)        
-        x = torch.cat([x, conv1], dim=1)   
-
-        x = self.dconv_up1(x)
-
-        out = self.conv_last(x)
-
-        return out
-    
-    def double_conv(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, padding=1),
-            nn.ReLU(inplace=True)
-        )
-
     
 # RLE 디코딩 함수
 def rle_decode(mask_rle, shape):
