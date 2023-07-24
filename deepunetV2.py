4864ac042cf26ffb4412a82581af1c8935b3c86e
COOLHO <coolho123@knu.ac.kr>
2023-07-24 15:40:04 +0900

    Create deepunetV2.py

---
+++ deepunetV2.py
@@ -0,0 +1,133 @@
+import torch
+import torch.nn as nn
+
+class DeepUNetV2(nn.Module):
+    def __init__(self):
+        super(DeepUNetV2, self).__init__()
+        self.dconv_down1 = self.double_conv(3, 64)
+        self.dconv_down2 = self.double_conv(64, 64)
+
+        self.dconv_down3 = self.double_conv(64, 128)
+        self.dconv_down4 = self.double_conv(128, 128)
+
+        self.dconv_down5 = self.double_conv(128, 256)
+        self.dconv_down6 = self.double_conv(256, 256)
+
+        self.dconv_down7 = self.double_conv(256, 512)   
+        self.dconv_down8 = self.double_conv(512, 512)
+
+        self.dconv_down9 = self.double_conv(512, 1024)
+        self.dconv_down10 = self.double_conv(1024, 1024)   
+
+        self.dconv_down11 = self.double_conv(1024, 2048)
+        self.dconv_down12 = self.double_conv(2048, 2048)  
+
+        self.dconv_down13 = self.double_conv(2048, 4096)
+        self.dconv_down14 = self.double_conv(4096, 4096)  
+
+
+        self.maxpool = nn.MaxPool2d(2)
+
+
+        self.upconv1 = nn.ConvTranspose2d(4096, 2048, 2, 2, padding = 0)      
+
+        self.dconv_up1 = self.double_conv(4096, 2048)
+        self.dconv_up2 = self.double_conv(2048, 2048)
+
+        self.upconv2 = nn.ConvTranspose2d(2048, 1024, 2, 2, padding = 0)      
+
+        self.dconv_up3 = self.double_conv(2048, 1024)
+        self.dconv_up4 = self.double_conv(1024, 1024)
+
+        self.upconv3 = nn.ConvTranspose2d(1024, 512, 2, 2, padding = 0)      
+
+        self.dconv_up5 = self.double_conv(1024, 512)
+        self.dconv_up6 = self.double_conv(512, 512)
+
+        self.upconv4 = nn.ConvTranspose2d(512, 256, 2, 2, padding = 0) 
+
+        self.dconv_up7 = self.double_conv(512, 256)
+        self.dconv_up8 = self.double_conv(256, 256)
+
+        self.upconv5 = nn.ConvTranspose2d(256, 128, 2, 2, padding = 0)
+
+        self.dconv_up9 = self.double_conv(256, 128)
+        self.dconv_up10 = self.double_conv(128, 128)
+
+        self.upconv6 = nn.ConvTranspose2d(128, 64, 2, 2, padding = 0    )
+
+        self.dconv_up11 = self.double_conv(128, 64)
+        self.dconv_up12 = self.double_conv(64, 64)
+
+        self.conv_last = nn.Conv2d(64, 1, 1)
+
+    def forward(self, x):
+        conv1 = self.dconv_down1(x)
+        conv2 = self.dconv_down2(conv1)
+        x = self.maxpool(conv2)
+
+        conv3 = self.dconv_down3(x)
+        conv4 = self.dconv_down4(conv3)
+        x = self.maxpool(conv4)
+
+        conv5 = self.dconv_down5(x)
+        conv6 = self.dconv_down6(conv5)
+        x = self.maxpool(conv6)
+
+        conv7 = self.dconv_down7(x)
+        conv8 = self.dconv_down8(conv7)
+        x = self.maxpool(conv8)
+
+        conv9 = self.dconv_down9(x)    
+        conv10 = self.dconv_down10(conv9)
+        x = self.maxpool(conv10)
+
+        conv11 = self.dconv_down11(x)    
+        conv12 = self.dconv_down12(conv11)
+        x = self.maxpool(conv12)
+
+        conv13 = self.dconv_down9(x)    
+        conv14 = self.dconv_down10(conv13)
+
+
+        x = self.upconv1(conv14)       
+        x = torch.cat([x, conv12], dim=1)
+
+        x = self.dconv_up1(x)
+        x = self.dconv_up2(x)  
+        x = self.upconv2(x)     
+        x = torch.cat([x, conv10], dim=1)      
+
+        x = self.dconv_up3(x)
+        x = self.dconv_up4(x)
+        x = self.upconv3(x)      
+        x = torch.cat([x, conv8], dim=1)  
+
+        x = self.dconv_up5(x)
+        x = self.dconv_up6(x)   
+        x = self.upconv4(x)   
+        x = torch.cat([x, conv6], dim=1) 
+
+        x = self.dconv_up7(x)
+        x = self.dconv_up8(x)   
+        x = self.upconv5(x)   
+        x = torch.cat([x, conv4], dim=1) 
+
+        x = self.dconv_up9(x)
+        x = self.dconv_up10(x)   
+        x = self.upconv6(x)   
+        x = torch.cat([x, conv2], dim=1) 
+
+        x = self.dconv_up11(x)
+        x = self.dconv_up12(x) 
+
+        out = self.conv_last(x)
+
+        return out
+
+    def double_conv(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, padding=1, stride = 1, bias = True),
+            nn.BatchNorm2d(num_features=out_channels),
+            nn.ReLU(inplace=True),
+        )    
